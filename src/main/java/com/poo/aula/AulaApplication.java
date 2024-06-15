ae77f6c476601e874c777412bfe7059aab0bd19d
Arthur Giangiarulo <111184548+arthurGiangiarulo@users.noreply.github.com>
2024-06-14 21:18:39 -0300

    scripts para limpar console java

--- src/main/java/com/poo/aula/AulaApplication.java
+++ src/main/java/com/poo/aula/AulaApplication.java
@@ -9,8 +9,39 @@ public class AulaApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(AulaApplication.class, args);
 
+		limpa_mac_linux();
+
 		Lista01 lista = new Lista01();
 		lista.ex1();
+
+		finaliza();
 	}
 
+	static void limpa_mac_linux() {
+		try {
+            if (System.getProperty("os.name").contains("Linux") || System.getProperty("os.name").contains("Mac")) {
+                new ProcessBuilder("clear").inheritIO().start().waitFor();
+            } else {
+                System.out.println("Este comando só funciona no Linux ou Mac.");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+	}
+
+	static void limpa_windows() {
+		try {
+            if (System.getProperty("os.name").contains("Windows")) {
+                new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+            } else {
+                System.out.println("Este comando só funciona no Windows.");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+	}
+
+	static void finaliza() {
+		System.out.println("\n\n\n\n");
+	}
 }
